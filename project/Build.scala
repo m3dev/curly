4910d006c0ed870cebfd06c935db934694baef60
Kaz Sera <seratch@gmail.com>
2016-02-10 20:23:07 +0900

    SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object AppBuild extends Build {
 
-  val _version = "0.5.6"
+  val _version = "0.5.7-SNAPSHOT"
 
   lazy val libraryProject = Project(id = "library", base = file("."), settings = Seq(
     sbtPlugin := false,
